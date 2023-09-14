40a82e87d187e3003138f95b7844224275669a37
franzzle <me@franzzle.com>
2023-09-14 22:15:33 +0200

    transcribe wav file

---
+++ src/main/java/com/franzzle/tooling/lipsync/api/SpeechRecognitionApi.java
@@ -0,0 +1,14 @@
+package com.franzzle.tooling.lipsync.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "transcribing", description = "Handles transcribing")
+public interface SpeechRecognitionApi {
+    @Operation(summary = "Transcribe", tags = {"transcribing"})
+    @PostMapping("/transcribe")
+    ResponseEntity<String> transcribeAudio(@RequestParam("uuid") String uuid);
+}
---
+++ src/main/java/com/franzzle/tooling/lipsync/api/SpeechRecognitionApiImpl.java
@@ -0,0 +1,53 @@
+package com.franzzle.tooling.lipsync.api;
+
+import com.franzzle.tooling.lipsync.api.service.ConvertedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/asr")
+public class SpeechRecognitionApiImpl {
+    @Autowired
+    private ConvertedService convertedService;
+    @Value("${speech.recognition.api.url}")
+    private String speechRecognitionApiUrl;
+
+    @PostMapping("/transcribe")
+    public ResponseEntity<String> transcribeAudio(@RequestParam("uuid") String uuid) {
+        try {
+            // Use the UUID to retrieve the JSON result as a Resource
+            Resource audioResource = convertedService.getResultingWavInput(String.format("%s.wav", uuid));
+
+            MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
+            bodyBuilder.part("audio_file", audioResource)
+                    .header("Content-Type", "audio/x-wav");
+
+            // Set up the RestTemplate
+            RestTemplate restTemplate = new RestTemplate();
+
+            // Make a POST request to the external API
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    speechRecognitionApiUrl + "?task=transcribe&language=en&encode=true&output=txt",
+                    bodyBuilder.build(),
+                    String.class
+            );
+
+            // Implement your logic to process the JSON resource if needed
+            // ...
+            System.out.println(response.getBody());
+
+            return ResponseEntity.ok(response.getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("Error processing the request.");
+        }
+    }
+}
