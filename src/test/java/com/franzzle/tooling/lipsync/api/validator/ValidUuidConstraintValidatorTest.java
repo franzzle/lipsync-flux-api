e9a06f2db5ee627a04d904e0f3ac4f71ee2afc10
Frans Kramer <franzzle@franzzle.com>
2023-10-10 14:22:59 +0200

    added test

---
+++ src/test/java/com/franzzle/tooling/lipsync/api/validator/ValidUuidConstraintValidatorTest.java
@@ -0,0 +1,32 @@
+package com.franzzle.tooling.lipsync.api.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import javax.validation.ConstraintValidatorContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class ValidUuidConstraintValidatorTest {
+
+    private ValidUuidConstraintValidator validUuidConstraintValidator;
+    private ConstraintValidatorContext constraintValidatorContext;
+
+    @BeforeEach
+    void setUp() {
+        validUuidConstraintValidator = new ValidUuidConstraintValidator();
+        constraintValidatorContext = mock(ConstraintValidatorContext.class);
+    }
+
+    @Test
+    void testIsValid() {
+        assertTrue(validUuidConstraintValidator.isValid("3d490a70-94ce-4d15-9494-5248280eebb3", constraintValidatorContext));
+        assertFalse(validUuidConstraintValidator.isValid("not-valid-uuid", constraintValidatorContext));
+    }
+
+    @Test
+    void testIsUUID() {
+        assertTrue(ValidUuidConstraintValidator.isUUID("3d490a70-94ce-4d15-9494-5248280eebb3"));
+        assertFalse(ValidUuidConstraintValidator.isUUID("not-valid-uuid"));
+    }
+}
